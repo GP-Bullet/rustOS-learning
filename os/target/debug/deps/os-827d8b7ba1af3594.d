6acf66546260be36460c245e12a7eb8f757407db
GP-Bullet <1285719445@qq.com>
2022-11-17 22:52:41 +0800

    updata

---
+++ os/target/debug/deps/os-827d8b7ba1af3594.d
@@ -0,0 +1,6 @@
+/home/gaopeng/Desktop/rustOS-learning/os/target/debug/deps/os-827d8b7ba1af3594: src/main.rs src/lang_items.rs
+
+/home/gaopeng/Desktop/rustOS-learning/os/target/debug/deps/os-827d8b7ba1af3594.d: src/main.rs src/lang_items.rs
+
+src/main.rs:
+src/lang_items.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-8b37b477f3fd5f35.d
@@ -0,0 +1,6 @@
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-8b37b477f3fd5f35: src/main.rs src/lang_items.rs
+
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-8b37b477f3fd5f35.d: src/main.rs src/lang_items.rs
+
+src/main.rs:
+src/lang_items.rs:
