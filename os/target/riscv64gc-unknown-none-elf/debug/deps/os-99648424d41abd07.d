98a57641ead012f5d5c59d7f3987fa3fe2d78d85
GP-Bullet <1285719445@qq.com>
2022-11-18 19:54:41 +0800

    zhiling

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-99648424d41abd07.d
@@ -0,0 +1,7 @@
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-99648424d41abd07.rmeta: src/main.rs src/lang_items.rs src/entry.asm
+
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-99648424d41abd07.d: src/main.rs src/lang_items.rs src/entry.asm
+
+src/main.rs:
+src/lang_items.rs:
+src/entry.asm:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-d7fd1f69ab4e74d3.d
@@ -0,0 +1,7 @@
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-d7fd1f69ab4e74d3.rmeta: src/main.rs src/lang_items.rs src/entry.asm
+
+/home/gaopeng/Desktop/rustOS-learning/os/target/riscv64gc-unknown-none-elf/debug/deps/os-d7fd1f69ab4e74d3.d: src/main.rs src/lang_items.rs src/entry.asm
+
+src/main.rs:
+src/lang_items.rs:
+src/entry.asm:
